921fd4afef654fb19d241f5d58536df13782df82
dotfinance-dev <83828384+dotfinance-dev@users.noreply.github.com>
2021-06-07 23:30:04 +0300

    Add files via upload

--- contracts/dot/contracts/interfaces/IVaultController.sol
+++ contracts/dot/contracts/interfaces/IVaultController.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.6.12;
 * MIT License
 * ===========
 *
-* Copyright (c) 2021 PinkFinance
+* Copyright (c) 2021 DotFinance
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
